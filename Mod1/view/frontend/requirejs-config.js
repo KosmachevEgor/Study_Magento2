ea73907a3e3faa6f7bcface3de9a4a8bbd605d82
KosmachevEgor <Egor_Kosmachev.2003@mail.ru>
2023-08-16 12:13:49 +0300

    Upgrading the old Study_Mod1 repository to the extended Magento2_Study

---
+++ Mod1/view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Study_Mod1/js/borderColor': {
+                'Study_Mod1/js/borderColor-mixin': true
+            }
+        }
+    }
+}
---
+++ Mod1/view/frontend/web/js/borderColor-mixin.js
@@ -0,0 +1,13 @@
+define(['jquery'], function ($){
+   var widgetMixin = {
+       changeBorderColor: function () {
+           $(".studyid-index-index").find('.form-timer').css({'border': 'solid 10px #00edff'});
+           this._super();
+       }
+   };
+
+   return function (targetWidget) {
+       $.widget('studynamespace.borderWidget', targetWidget, widgetMixin);
+       return $.studynamespace.borderWidget;
+   }
+})
