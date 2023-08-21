5da34b078680d37c3abf84f0ea15744bc0fb89c3
KosmachevEgor <Egor_Kosmachev.2003@mail.ru>
2023-08-21 19:19:49 +0300

    Additional task

---
+++ Mod1/Plugin/Mod2/Observer/ChangePromoSkuObserver.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Study\Mod1\Plugin\Mod2\Observer;
+
+use Study\Mod2\Observer\PromoSkuObserver;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Event\Observer;
+
+class ChangePromoSkuObserver
+{
+    private RequestInterface $request;
+
+    public function __construct(
+        RequestInterface $request
+    ) {
+        $this->request = $request;
+    }
+
+    public function aroundExecute(PromoSkuObserver $subject, callable $proceed, Observer $observer)
+    {
+        return $this->getRequest()->isXmlHttpRequest() ? null : $proceed($observer);
+    }
+
+    private function getRequest(): RequestInterface
+    {
+        return $this->request;
+    }
+}
